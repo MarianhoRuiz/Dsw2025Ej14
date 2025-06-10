f0feb17aa8b3480ab12f7895e0f585f32238f343
MarianoRuiz <marianoruizcoronel@gmail.com>
2025-06-10 16:58:45 -0300

    Trabajo en clase

---
+++ Dsw2025Ej14.Api/Dsw2025Ej14.Api/Data/PersistenciaEnMemoria.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using Dsw2025Ej14.Domain;
+
+namespace Dsw2025Ej14.Api.Data
+{
+    public class PersistenciaEnMemoria
+    {
+        private const string Archivo = "Products.json";
+        private List<Product> _productos = new();
+
+        public void LoadProducts()
+        {
+            if (File.Exists(Archivo))
+            {
+                string json = File.ReadAllText(Archivo);
+                var productos = JsonSerializer.Deserialize<List<Product>>(json);
+                if (productos != null)
+                {
+                    _productos = productos;
+                }
+            }
+        }
+
+        public List<Product> ObtenerProductos()
+        {
+            return _productos;
+        }
+    }
+}
---
+++ Dsw2025Ej14.Api/Dsw2025Ej14.Api/Domain/Entidades/Product.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Dsw2025Ej14.Domain
+{
+    public class Product
+    {
+        private string _sku;
+        private string _name;
+        private decimal _currentUnitPrice;
+        private bool _IsActive;
+
+        public Product(string sku, string name, decimal currentUnitPrice, bool isActive)
+        {
+            Sku = sku;
+            Name = name;
+            CurrentUnitPrice = currentUnitPrice;
+            IsActive = isActive;
+        }
+
+        public string Sku { get => _sku; set => _sku = value; }
+        public string Name { get => _name; set => _name = value; }
+        public decimal CurrentUnitPrice { get => _currentUnitPrice; set => _currentUnitPrice = value; }
+        public bool IsActive { get => _IsActive; set => _IsActive = value; }
+    }
+}
